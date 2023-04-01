4e112d38b73a475421039fcee6ba90d47424c836
Harsimran sidhu <22066581+PKHarsimran@users.noreply.github.com>
2023-04-01 15:57:48 +0000

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,10 +1,28 @@
 function toggleDarkMode() {
+  var themeToggle = document.getElementById('themeToggle');
   document.body.classList.toggle('dark-mode');
+
+  if (document.body.classList.contains('dark-mode')) {
+    themeToggle.innerHTML = '☀️';
+    themeToggle.setAttribute('data-theme', 'dark');
+  } else {
+    themeToggle.innerHTML = '🌙';
+    themeToggle.setAttribute('data-theme', 'light');
+  }
+
   localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+window.onload = function () {
+  var themeToggle = document.getElementById('themeToggle');
+
   if (localStorage.getItem('dark-mode') === 'true') {
     document.body.classList.add('dark-mode');
+    themeToggle.innerHTML = '☀️';
+    themeToggle.setAttribute('data-theme', 'dark');
+  } else {
+    document.body.classList.remove('dark-mode');
+    themeToggle.innerHTML = '🌙';
+    themeToggle.setAttribute('data-theme', 'light');
   }
-});
+};
